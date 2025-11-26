da1c5322cc9630fe6de881a5591516ad00fc55c6
Ervin Püsijainen <ervinpusijainen@gmail.com>
2025-11-26 16:52:25 +0200

    kolm funktsiooni lisatud

--- content/matemaatilised.php
+++ content/matemaatilised.php
@@ -26,3 +26,39 @@ echo $arv1. "- vähendamine ühe võrra (arv1--;)";
 echo "<br>";
 echo "<strong>Ruutjuur -sqrt(): </strong>".sqrt($arv1);
 echo "<br>";
+echo "<h3>Keskmise arvutamine</h3>";
+
+echo "<br>";
+echo "<strong>Massiivi [1, 2, 3, 4, 5] keskmine: </strong>";
+function keskmine($massiiv)
+{
+    $summa = 0;
+    foreach ($massiiv as $number)
+    {
+        $summa += $number;
+    }
+
+    return $summa / count($massiiv);
+}
+
+echo keskmine([1, 2, 3, 4, 5]);
+
+echo "<br>";
+echo "<h3> Juhusliku arvu tagastamine</h3>";
+echo "<br>";
+
+echo "<strong> Juhuslik arv rand(): </strong>";
+echo rand();
+echo "<br>";
+echo "<strong>Vahemikus 1-10 rand(1, 10): </strong>";
+echo rand(1, 10);
+echo "<br>";
+echo "<h3>Protsendi arvutamine</h3>";
+echo "<br>";
+echo "<strong>47.5% arvust 100: </strong>";
+function protsendid($arv, $protsent)
+{
+    return ($arv / 100) * $protsent;
+}
+
+echo protsendid(100, 47.5);
